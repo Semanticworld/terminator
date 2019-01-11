34e2f9e4e0caf7fd91b19efa933efb6f9bb69dd2
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-11 14:44:41 +0200

    Update livacha.js

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -21,7 +21,7 @@ $(document).ready(function () {
     };
 
     Terminator.prototype.initSmailes = function () {
-        $("div.chat-users-options.em-smiley").before('<div class="col text-left">some text</div>');
+        $("i.em-smiley").before('<div class="col text-left">some text</div>');
         console.log("ready");
 
         
