51cdfd482240321ae20a98a26529c0999ae4d697
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-11 12:51:42 +0200

    Update livacha.js

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -1,30 +1,43 @@
-$( document ).ready(function() {
-function Terminator() {
-    this.version = "0.0.0.1";
-    this.home = "https://raw.githubusercontent.com/Semanticworld/terminator/master/assets/";
-}
-Terminator.prototype.run = function () {
-    console.log(this.version);
-    this.loadjs(this.home + "plugins/semantic/semantic.min.js");
-};
-Terminator.prototype.loadjs = function (url) {
-    var xhr = new XMLHttpRequest();
-    xhr.open("get", url, true);
-    xhr.onreadystatechange = function () {
-        if (xhr.readyState == 4) {
-            if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
-                var script = document.createElement("script");
-                script.type = "text/javascript";
-                script.text = xhr.responseText;
-                document.body.appendChild(script);
-            }
-        }
+$(document).ready(function () {
+    function Terminator() {
+        this.version = "0.0.0.1";
+        this.home = "https://raw.githubusercontent.com/Semanticworld/terminator/master/assets/";
+    }
+    Terminator.prototype.run = function () {
+        console.log(this.version);
+        this.loadcss(this.home + "plugins/semantic/semantic.min.css");
+        this.loadjs(this.home + "plugins/semantic/semantic.min.js");
     };
-    xhr.send(null);
-}
-
+    Terminator.prototype.loadjs = function (url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("get", url, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
+                    var script = document.createElement("script");
+                    script.type = "text/javascript";
+                    script.text = xhr.responseText;
+                    document.body.appendChild(script);
+                }
+            }
+        };
+        xhr.send(null);
+    }
+    Terminator.prototype.loadcss = function (url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("get", url, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
+                    var script = document.createElement("script");
+                    script.type = "text/javascript";
+                    script.text = xhr.responseText;
+                    document.body.appendChild(script);
+                }
+            }
+        };
+        xhr.send(null);
+    }
     var Terminator = new Terminator();
     Terminator.run();
 });
-
-
