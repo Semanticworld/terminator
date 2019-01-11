4b97149de0821c9ace9672966c2195972e90c08a
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-11 11:15:02 +0200

    add terminators

--- terminator.user.js
+++ terminator.user.js
@@ -2,7 +2,7 @@
 // @name        Terminator
 // @description Terminator - Chat optimizator
 // @author      Semanticworld
-// @version     0.0.0.9
+// @version     0.0.0.10
 // @updateURL   https://github.com/Semanticworld/terminator/raw/master/terminator.user.js
 // @icon        https://github.com/Semanticworld/terminator/raw/master/assets/ico/ico128.png
 // @icon64      https://github.com/Semanticworld/terminator/raw/master/assets/ico/ico64.png
