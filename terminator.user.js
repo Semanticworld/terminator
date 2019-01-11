7b23ab1be436617af3dbca0540bfc4bb1c0fec84
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-11 10:08:41 +0200

    Update terminator.user.js

--- terminator.user.js
+++ terminator.user.js
@@ -14,6 +14,14 @@
 // ==/UserScript==
 (function() {
     'use strict';
+        function getHostName(url) {
+        var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
+        if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
+            return match[2];
+        } else {
+            return null;
+        }
+    }
     var dom = getHostName(document.location.href);
     console.log("test good");
 });
