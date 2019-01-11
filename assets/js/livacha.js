39a910a8203a01d0d62221cb70b4ff9cee871fa9
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-11 16:53:57 +0200

    Update livacha.js

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -5,55 +5,65 @@ $(document).ready(function () {
     }
     Terminator.prototype.run = function () {
         console.log(this.version);
-        this.loadcss(this.home+"plugins/jquery-ui/jquery-ui.css");
-        this.loadjs(this.home+"plugins/jquery-ui/jquery-ui.min.js");
+        this.loadcss(this.home + "plugins/jquery-ui/jquery-ui.css");
+        this.loadjs(this.home + "plugins/jquery-ui/jquery-ui.min.js");
 
-        
 
-        var waitPanel = setInterval(function() {
+
+        var waitPanel = setInterval(function () {
             console.log("wait");
-            if ($("i.em-smiley").length){
+            if ($("i.em-smiley").length) {
                 clearInterval(waitPanel);
                 Terminator.initSmailes();
             }
-          }, 2000);
+        }, 2000);
 
-        
+
     };
 
     Terminator.prototype.initSmailes = function () {
-var m="<div id='ter-smiles' class='ui-widget-content'>"+
-"<div id='accordion'>"+
-  "<h3>Section 1</h3>"+
-  "<div><p>Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.</p></div>"+
-  "<h3>Section 2</h3>"+
-  "<div><p>Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In suscipit faucibus urna. </p></div>"+
-  "<h3>Section 3</h3>"+
-  "<div><p>Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis. Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui. </p><ul><li>List item one</li>    <li>List item two</li>      <li>List item three</li>    </ul>  </div>"+
-  "<h3>Section 4</h3>"+
-  "<div><p>Cras dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia mauris vel est. </p><p>Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p></div>"+
-"</div>"+
-"</div>";
-        $("i.em-smiley").parent().parent().before('<div class="col text-left"><span data-trigger="click" data-container="body" data-html="true" data-toggle="popover" data-placement="left" data-content="'+m+'" class="toggler cursor-pointer smiles mr-3"><i class="em em-nerd_face"></i></span></div>');
-
-        
+        var m = "<div id='ter-smiles' class='ui-widget-content'>" +
+            "<div id='accordion'>" +
+            "<h3>Section 1</h3>" +
+            "<div><p>Mauris mauris ante, blandit et, ultrices a, suscipit eget, quam. Integer ut neque. Vivamus nisi metus, molestie vel, gravida in, condimentum sit amet, nunc. Nam a nibh. Donec suscipit eros. Nam mi. Proin viverra leo ut odio. Curabitur malesuada. Vestibulum a velit eu ante scelerisque vulputate.</p></div>" +
+            "<h3>Section 2</h3>" +
+            "<div><p>Sed non urna. Donec et ante. Phasellus eu ligula. Vestibulum sit amet purus. Vivamus hendrerit, dolor at aliquet laoreet, mauris turpis porttitor velit, faucibus interdum tellus libero ac justo. Vivamus non quam. In suscipit faucibus urna. </p></div>" +
+            "<h3>Section 3</h3>" +
+            "<div><p>Nam enim risus, molestie et, porta ac, aliquam ac, risus. Quisque lobortis. Phasellus pellentesque purus in massa. Aenean in pede. Phasellus ac libero ac tellus pellentesque semper. Sed ac felis. Sed commodo, magna quis lacinia ornare, quam ante aliquam nisi, eu iaculis leo purus venenatis dui. </p><ul><li>List item one</li>    <li>List item two</li>      <li>List item three</li>    </ul>  </div>" +
+            "<h3>Section 4</h3>" +
+            "<div><p>Cras dictum. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Aenean lacinia mauris vel est. </p><p>Suspendisse eu nisl. Nullam ut libero. Integer dignissim consequat lectus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. </p></div>" +
+            "</div>" +
+            "</div>";
 
 
+        var d = '<div id="dialog" title="Basic dialog"><p>This is an animated dialog which is useful for displaying information. The dialog window can be moved, resized and closed with the x icon.</p></div>';
 
-        
-            $( "#accordion" ).accordion({
-              heightStyle: "fill"
-            });
-         
-            $( "#ter-smiles" ).resizable({
-              minHeight: 140,
-              minWidth: 200,
-              resize: function() {
-                $( "#accordion" ).accordion( "refresh" );
-              }
-            });
-          
-$('[data-toggle="popover"]').popover();
+
+
+        $("i.em-smiley")
+            .parent()
+            .parent()
+            .before('<div class="col text-left"><span data-trigger="click" class="toggler cursor-pointer smiles mr-3"><i class="em em-nerd_face"></i></span></div>').
+        before(d);
+
+
+
+
+
+
+        $("#accordion").accordion({
+            heightStyle: "fill"
+        });
+
+        $("#ter-smiles").resizable({
+            minHeight: 140,
+            minWidth: 200,
+            resize: function () {
+                $("#accordion").accordion("refresh");
+            }
+        });
+
+        $('[data-toggle="popover"]').popover();
 
 
     }
