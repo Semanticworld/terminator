7904727a9001f767b8a55cedaffd63883a9294e1
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-11 11:12:31 +0200

    Update terminator.user.js

--- terminator.user.js
+++ terminator.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name        Terminator
-// @description Terminator - chat optimizator
+// @description Terminator - Chat optimizator
 // @author      Semanticworld
-// @version     0.0.0.8
+// @version     0.0.0.9
 // @updateURL   https://github.com/Semanticworld/terminator/raw/master/terminator.user.js
 // @icon        https://github.com/Semanticworld/terminator/raw/master/assets/ico/ico128.png
 // @icon64      https://github.com/Semanticworld/terminator/raw/master/assets/ico/ico64.png
@@ -15,9 +15,10 @@
 // @grant       GM_xmlhttpRequest
 // @run-at      document-end
 // ==/UserScript==
-(function() {
+(function () {
     'use strict';
-        function getHostName(url) {
+
+    function getHostName(url) {
         var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
         if (match != null && match.length > 2 && typeof match[2] === 'string' && match[2].length > 0) {
             return match[2];
@@ -25,7 +26,30 @@
             return null;
         }
     }
+
+    function LoadSelectedTerminator(url) {
+        var xhr = new XMLHttpRequest();
+        xhr.open("get", url, true);
+        xhr.onreadystatechange = function () {
+            if (xhr.readyState == 4) {
+                if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
+                    var script = document.createElement("script");
+                    script.type = "text/javascript";
+                    script.text = xhr.responseText;
+                    document.body.appendChild(script);
+                }
+            }
+        };
+        xhr.send(null);
+    }
+    var home = "https://github.com/Semanticworld/terminator/raw/master/assets/js/";
     var dom = getHostName(document.location.href);
-    //console.log(dom);
-    alert("test");
+    switch (dom) {
+        case 'livacha.com':
+            LoadSelectedTerminator(home + "livacha.js?r=" + Math.random());
+            break;
+        case 'marshat.com':
+            LoadSelectedTerminator(home + "marshat.js?r=" + Math.random());
+            break;
+    }
 })();
