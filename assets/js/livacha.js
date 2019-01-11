70c1999b109af20e9721e6c993afe055ecffc553
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-11 15:48:29 +0200

    Update livacha.js

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -17,8 +17,8 @@ $(document).ready(function () {
     };
 
     Terminator.prototype.initSmailes = function () {
-        var t='<button type="button" class="btn btn-secondary" data-container="body" data-toggle="popover" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus.">Popover on top      </button>';
-        $("i.em-smiley").parent().parent().before('<div class="col text-left"><span class="toggler cursor-pointer smiles mr-3"><i class="em em-nerd_face"></i>'+t+'</span></div>');
+
+        $("i.em-smiley").parent().parent().before('<div class="col text-left"><span data-container="body" data-toggle="popover" data-placement="top" data-content="Vivamus sagittis lacus vel augue laoreet rutrum faucibus." class="toggler cursor-pointer smiles mr-3"><i class="em em-nerd_face"></i></span></div>');
 
         $('[data-toggle="popover"]').popover();
     }
