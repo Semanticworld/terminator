a0600219ea263ee3ebc78cb93f7627b3613f0318
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-11 11:44:53 +0200

    Update livacha.js

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -1,8 +1,27 @@
-function Terminator(){
+function Terminator() {
     this.version = "0.0.0.1";
+    this.home = "https://raw.githubusercontent.com/Semanticworld/terminator/master/assets/";
 }
-Terminator.prototype.run = function() {
+Terminator.prototype.run = function () {
     console.log(this.version);
-  };
-  var Terminator = new Terminator();
-  Terminator.run();
+    this.loadjs(this.home + "semantic/semantic.min.js");
+};
+Terminator.prototype.loadjs = function (url) {
+    var xhr = new XMLHttpRequest();
+    xhr.open("get", url, true);
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState == 4) {
+            if (xhr.status >= 200 && xhr.status < 300 || xhr.status == 304) {
+                var script = document.createElement("script");
+                script.type = "text/javascript";
+                script.text = xhr.responseText;
+                document.body.appendChild(script);
+            }
+        }
+    };
+    xhr.send(null);
+}
+
+
+var Terminator = new Terminator();
+Terminator.run();
