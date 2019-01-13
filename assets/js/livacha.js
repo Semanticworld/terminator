f9431cd925339187a392e1786d9dd222d9ee7ce0
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-13 12:29:41 +0200

    Update livacha.js

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -2,6 +2,7 @@ $(document).ready(function () {
     function Terminator() {
         this.version = "0.0.0.2";
         this.home = "https://raw.githubusercontent.com/Semanticworld/terminator/master/assets/";
+        this.lovesmiles=[];
     }
     Terminator.prototype.run = function () {
         console.log(this.version);
