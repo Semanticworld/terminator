fd61a5861fd7eb5bdf8464c5596c5804c573e10a
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-13 13:27:21 +0200

    Update livacha.js

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -115,7 +115,7 @@ $(document).ready(function () {
                 lovesmiles.shift();
                 $('.tr-tab-content.tr-sm0-block img').last().remove();
             };
-            $(".chat-layout-container .textarea-wrapper textarea").val(' *' + sm + '* ');
+            $(".chat-layout-container .textarea-wrapper textarea").val($(".chat-layout-container .textarea-wrapper textarea").val()+' *' + sm + '* ');
         });
 
         var lovesmiles = [];
