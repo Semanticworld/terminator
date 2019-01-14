4e28f125618cb3681466e0cad73516e94a72b423
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-15 00:57:22 +0200

    update smp 3

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -105,6 +105,7 @@ $(document).ready(function () {
         ];
         smilespack["3"] = [
             "aa", "ab", "ac", "ad", "ae",
+            "ba", "bb", "bc", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bk", "bl", "bm", "bn", "bo", "bp", "bq", "br", "bs", "bt", "bu", "bv", "bw", "bx", "by", "bz",
             "za"
         ];
 
