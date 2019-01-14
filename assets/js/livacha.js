e0bdbbe96c20eb3af517273af4de1b6314cdc6f1
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-15 01:16:21 +0200

    update cp 3

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -106,6 +106,7 @@ $(document).ready(function () {
         smilespack["3"] = [
             "aa", "ab", "ac", "ad", "ae",
             "ba", "bb", "bc", "bd", "be", "bf", "bg", "bh", "bi", "bj", "bk", "bl", "bm", "bn", "bo", "bp", "bq", "br", "bs", "bt", "bu", "bv", "bw", "bx", "by", "bz",
+            "ca", "cb", "cc", "cd", "ce", "cf", "cg", "ch", "ci", "cj", "ck", "cl", "cm", "cn", "co", "cp", "cq", "cr", "cs", "ct", "cu", "cv", "cw", "cx", "cy", "cz",
             "za"
         ];
 
