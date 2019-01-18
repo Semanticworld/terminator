d434826f707c26421730f9cd84bea6b431079d7e
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-18 13:15:25 +0200

    Update livacha.js

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -712,7 +712,7 @@ $(document).ready(function () {
 
 
         $(document).on("click", "button", function (e) {
-            console.log(e.Event);
+            console.log(e);
 
             var t = $(this).parents().parents().parents().parents().parents().parents().attr('class');
             t = t.split(" ");
