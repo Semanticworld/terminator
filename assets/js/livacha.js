7679417e4c279b5a5aa222802e729afd37f37e05
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-22 18:47:53 +0200

    upd smp 2

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -172,7 +172,7 @@ $(document).ready(function () {
             "aa", "ab", "ac", "ad", "ae", "af", "ag", "ah", "ai", "aj", "ak", "al", "am", "an", "ao", "ap", "ar", "at", "au", "av", "aw", "ax", "ay", "az",
             "ba", "bb", "bc", "bd", "be", "bf", "bg", "bh", "bi", "bj",
             "ca", "cb", "cc", "cd", "ce", "cf", "cg", "ch", "ci", "cj", "ck", "cl", "cm", "cn", "co", "cp", "cq", "cr", "cs", "ct", "cu", "cv", "cw", "cx", "cy", "cz",
-            "da", "db", "dc", "dd", "de"
+            "da", "db", "dc", "dd", "de", "df", "dg", "dh", "di", "dj", "dk", "dl", "dm", "dn", "do", "dp", "dq", "dr", "ds", "dt", "du", "dv", "dw", "dx", "dy", "dz"
         ];
         smilespack["3"] = [
             "aa", "ab", "ac", "ad", "ae",
