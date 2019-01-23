f95403ede0a85d5841fa5ca28a74a808cc8a26b8
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-23 12:15:03 +0200

    upd resizer

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -124,6 +124,11 @@ $(document).ready(function () {
             $(".tr-chatovod").prepend('<iframe src="//semantic.chatovod.ru/" frameborder="0" marginheight="0" marginwidth="0" width="100%" height="360"></iframe>');
 
 
+            $('.tr-tempchat').sizeChanged(function(){
+                console.log(this);
+            })
+
+
         }
     }
     Terminator.prototype.createSmilesBox = function () {
