7273d32366b37e5b21c852c73aa5aed8285fe5df
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-01-23 12:24:54 +0200

    Update livacha.js

--- assets/js/livacha.js
+++ assets/js/livacha.js
@@ -125,7 +125,7 @@ $(document).ready(function () {
 
 
             $('.tr-tempchat').sizeChanged(function(){
-                console.log(height);
+               // console.log(height);
             })
 
 
