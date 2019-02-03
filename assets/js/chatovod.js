a2a00e104f02c91e8a234f356d6f5ffabac0391c
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-02-03 15:07:14 +0200

    Update chatovod.js

--- assets/js/chatovod.js
+++ assets/js/chatovod.js
@@ -18,17 +18,32 @@ t.repl = function (str, f, r) {
     return l.split("*").join("");
 }
 
+t.messages = function (o) {
+    var $m = $(o);
+    var h = $(o).html();
+    if (h != undefined) {
+        var a = h.match(/\*...\*/g);
+        if (a != null) {
+            a.forEach(function (a) {
+                var z = a.split("*").join("/*");
+                var ext = 'gif';
+                var l = '<img data-face="' + a[1] + "-" + a[2] + a[3] + '" class="tr-smilex" src="' + t.home + 'smiles/s' + a[1] + '/' + a[2] + a[3] + '.' + ext + '">';
+                h = t.repl(h, z, l);
+            });
+            $($m).html(h);
+        }
+    }
+}
 
-
-t.start=function(){
+t.start = function () {
     $(document).ready(function () {
-    $('div.chatMessage').each(function(i, ele) {
-        console.log(i + ': ' + $(ele).html());  
-        // 0: <div id="outer"><div id="inner"></div></div>
-        // 1: <div id="inner"></div>
-    })
+        $('div.chatMessage').each(function (i, ele) {
+            console.log(i + ': ' + $(ele).html());
+            // 0: <div id="outer"><div id="inner"></div></div>
+            // 1: <div id="inner"></div>
+        })
 
-});
+    });
 }
 
 t.run = function () {
@@ -38,21 +53,7 @@ t.run = function () {
             console.log(this);
         });
         $(document).on('DOMNodeInserted', "div.chatMessage", function (e) {
-            var $m = $(this);
-            var h = $(this).html();
-            if (h != undefined) {
-                var a = h.match(/\*...\*/g);
-                if (a != null) {
-                    a.forEach(function (a) {
-                        var z = a.split("*").join("/*");
-                        console.log(a);
-                        var ext = 'gif';
-                        var l = '<img data-face="' + a[1] + "-" + a[2] + a[3] + '" class="tr-smilex" src="' + t.home + 'smiles/s' + a[1] + '/' + a[2] + a[3] + '.' + ext + '">';
-                        h = t.repl(h, z, l);
-                    });
-                    $($m).html(h);
-                }
-            }
+            t.messages(this);
         });
     });
 }
