30c1bf66340c660717676bfdcdc03fd7de8af707
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-02-03 17:56:35 +0200

    Update popupwindow.js

--- assets/plugins/popup/popupwindow.js
+++ assets/plugins/popup/popupwindow.js
@@ -197,7 +197,7 @@ PopupWindow - The ultimate popup/dialog/modal jQuery plugin
     
     // Minimized Area
     var _minimizedArea = {
-        position    : "top left",
+        position    : "bottom left",
         direction   : "vertical"
     };
     
