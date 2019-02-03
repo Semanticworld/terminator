49a3bf0c4bb6560e12e84d5723291cc5e609ffc2
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-02-03 15:44:08 +0200

    Update chatovod.js

--- assets/js/chatovod.js
+++ assets/js/chatovod.js
@@ -38,5 +38,6 @@ t.run = function () {
         $(document).on('DOMNodeInserted', "div.chatMessage", function () {
             t.messages(this);
         });
+        $(".chatLinkSmiles").appendChild('<a href="#" class="chatLinkSmiles"><span>смайлы</span></a>');
     });
 }
