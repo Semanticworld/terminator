f9af12da8bce09857f48a906db36bc087e8ec8b9
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-02-03 14:07:39 +0200

    add chatovod  support

--- terminator.user.js
+++ terminator.user.js
@@ -2,13 +2,14 @@
 // @name        Terminator
 // @description Terminator - Chat optimizator
 // @author      Semanticworld
-// @version     0.0.0.20
+// @version     0.0.0.21
 // @updateURL   https://semanticworld.github.io/terminator/terminator.user.js
 // @icon        https://semanticworld.github.io/terminator/assets/ico/ico128.png
 // @icon64      https://semanticworld.github.io/terminator/assets/ico/ico64.png
 // @namespace   https://livacha.com/
 // @match       https://livacha.com/*
 // @match       https://dropmefiles.com/*
+// @match       *.chatovod.ru*
 // @connect     livacha.com
 // @connect     dropmefiles.com
 // @require     https://cdnjs.cloudflare.com/ajax/libs/crypto-js/3.1.9-1/crypto-js.min.js
@@ -19,7 +20,7 @@
     'use strict';
     var home = 'https://semanticworld.github.io/terminator/assets/';
     localStorage.setItem('tr-home', home);
-    localStorage.setItem('tr-ver', '0.0.0.20');
+    localStorage.setItem('tr-ver', '0.0.0.21');
 
     function getHostName(url) {
         var match = url.match(/:\/\/(www[0-9]?\.)?(.[^/:]+)/i);
@@ -51,6 +52,12 @@
             break;
         case 'marshat.com':
             LoadSelectedTerminator(home + 'js/marshat.js?r=' + Math.random());
+            break;
+        default:
+            var sd = (document.location.href).split(".");
+            if (sd[1] == "chatovod") {
+                LoadSelectedTerminator(home + 'js/chatovod.js?r=' + Math.random());
+            }
             break;
     }
 })();
