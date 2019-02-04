cac091463ef9d2a673f13661d0c811e4c70170a2
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-02-04 16:11:21 +0200

    Update chatovod.js

--- assets/js/chatovod.js
+++ assets/js/chatovod.js
@@ -88,12 +88,15 @@ t.wininit = function () {
             }
             $('<div class="trcon">' + h1 + h2 + '</div>').appendTo('.trbody');
             t.loadjs("js", t.home + "js/smpack");
-            $(".tr-smile").hover(function () {
+          //  $(".tr-smile").hover(function () {
                 //  $( this ).fadeOut( 100 );
                 //  $( this ).fadeIn( 500 );
-                console.log($(this).attr('title'));
-                $("#trwin").PopupWindow("statusbar", $(this).attr('title'));
-            });
+          //      console.log($(this).attr('title'));
+          //      $("#trwin").PopupWindow("statusbar", $(this).attr('title'));
+          //  });
+          $( ".tr-smile" ).bind( "mouseenter mouseleave", function() {
+            console.log(1);
+          });
 
         } else {
             setTimeout(wait, 100);
