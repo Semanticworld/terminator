037536f57ac6b8441cc0cb63a8edcd1c6c53c68f
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-02-04 14:59:18 +0200

    Update smpack.js

--- assets/js/smpack.js
+++ assets/js/smpack.js
@@ -631,3 +631,31 @@ smpack["1"] = [
         "zn": "О как все это не прилично...",
         "zo": "Какая прикольная тёлочка..."
     };
+smpack.forEach(function (a, b) {
+    var s = '';
+    if (b == "8") {
+        for (var u in a) {
+            if (u.length > 2) {
+                s += '<hr><center>' + u + '</center>';
+            } else {
+                s += '<img title="' + a[u] + '" src="' + t.home + 'smiles/s8/aa.png" class="tr-smile" data-face="' + b + u + '">';
+            }
+        }
+    } else {
+        var ext = 'gif';
+        a.forEach(function (a) {
+            var ti = '';
+            if (b == "6") {
+                ext = 'png';
+            }
+            if (a.length > 2) {
+                s += '<hr><center>' + a + '</center>';
+            } else {
+                s += '<img src="' + t.home + 'ico/ld.gif" class="tr-smile lazyload" data-face="' + b + a + '" data-src="' + t.home + 'smiles/' + 's' + b + '/' + a + '.' + ext + '">';
+            }
+        });
+    }
+    $("div.trsec.trc" + b ).append(s);
+});
+
+ 
