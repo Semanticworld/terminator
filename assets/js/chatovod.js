96a504ead4c5d4ca76a00a79413ceb9ce4ee9cfb
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-02-21 05:05:47 +0200

    Update chatovod.js

--- assets/js/chatovod.js
+++ assets/js/chatovod.js
@@ -44,9 +44,10 @@ t.messages = function (o) {
 
 
                     if (typeof (jQuery().PopupWindow) == "function") {
-                        var i = '<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" width="362" height="280" type="text/html" src="https://www.youtube.com/embed/' + vid[1] + '?autoplay=1&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=1&start=0&end=0&origin=https://youtubeembedcode.com"></iframe>';
-                        $(".trframe").append(i);
                         $(t.player).PopupWindow("open");
+                        var i = '<iframe frameborder="0" scrolling="no" marginheight="0" marginwidth="0" width="362" height="280" type="text/html" src="https://www.youtube.com/embed/' + vid[1] + '?autoplay=1&fs=1&iv_load_policy=3&showinfo=0&rel=0&cc_load_policy=1&start=0&end=0&origin=https://youtubeembedcode.com"></iframe>';
+                        $(".trframe").appendTo(i);
+                        
                     }
                 }
             }
