8d3cc9153515da042c0e87c8e28fdd514dfa856d
Semanticworld <46590116+Semanticworld@users.noreply.github.com>
2019-02-21 06:25:47 +0200

    Update chatovod.js

--- assets/js/chatovod.js
+++ assets/js/chatovod.js
@@ -40,6 +40,7 @@ t.messages = function (o) {
             if (a != null) {
                 var link = atob(a);
                 var vid = link.match(/(?:youtube\.com\/(?:[^\/]+\/.+\/|(?:v|e(?:mbed)?)\/|.*[?&]v=)|youtu\.be\/)([^"&?\/ ]{11})/i);
+                console.log(vid[1].length );
                 if (vid[1].length > 5) {
                     if (typeof (jQuery().PopupWindow) == "function") {
                         $(t.player).PopupWindow("open");
